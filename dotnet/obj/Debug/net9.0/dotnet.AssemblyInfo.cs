e87904cbb7c9ce88219e72af77f8b08aa5cdf441
Anthony Salemo <ajssalemo@gmail.com>
2025-05-18 22:11:03 -0400

    adding gitignor and a new dotnet app

--- dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
+++ dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a09421a070bdd8e1dbc10c69d4667c339b915f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d5c493612af89a4d7eb3e296089bea14697b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
