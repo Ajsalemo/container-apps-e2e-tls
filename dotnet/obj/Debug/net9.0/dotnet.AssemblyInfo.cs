59651ffce39719899fb4a313c11ff7c34e53ef57
Anthony Salemo <ajssalemo@gmail.com>
2025-05-20 20:54:57 -0400

    containerized the .NET application

--- dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
+++ dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87904cbb7c9ce88219e72af77f8b08aa5cdf441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0558c044b091b6c90d1d5b21dea6f1177677b260")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
