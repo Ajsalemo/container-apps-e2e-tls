c8c87dd727ceb50c7966d4adfd8f9256e438ce8b
Anthony Salemo <ajssalemo@gmail.com>
2025-05-20 20:56:53 -0400

    updating README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # container-apps-e2e-tls
+
+This repository contains basic applications designed to run with TLS/HTTPS enabled web servers.
--- dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
+++ dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0558c044b091b6c90d1d5b21dea6f1177677b260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59651ffce39719899fb4a313c11ff7c34e53ef57")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
